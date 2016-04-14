bf8f8b1081f9c55a6787087d249ef6bc0d5070db
laznrbfe <laznrbfe@163.com>
2016-04-15 02:35:28 +0800

    修改push跳转页面

--- views/Channels.js
+++ views/Channels.js
@@ -10,7 +10,7 @@ import React,{
 
 import ZQMSVGButton from '../components/ZQMSVGButton';
 
-
+import Album from './Album';
 
 // 婚宴酒店  #f88     id:268,type:"wide"
 const SVG_PATH_HYJD = 'M21.9,13.2c-1.1-0.3-1.3-0.7-1.2-1.7c0-2.4-1.2-3.6-3.5-3.7c-0.7,0-1.4,0-2.3,0c1-1.2,1.9-2.1,2.6-3.1 c1-1.4,0.9-2.6,0-3.6c-1-1.1-2.3-1.2-3.7-0.2c-0.5,0.4-0.7,0.4-1.2,0c-1.4-1-2.7-0.9-3.7,0.2c-1,1-1,2.2,0,3.7 c0.7,1.1,1.6,1.9,2.7,3.1c-0.7,0-1.2,0-1.6,0c-3.4,0-4.3,0.9-4.4,4.3c0,0.8-0.4,0.8-1,1c-3.1,0.8-5.1,3.9-3.8,6.7 c0.8,1.7,0.9,3.3,0.9,5.1c0,1,0.4,1.2,1.2,1.2c6.8,0,13.6,0,20.4,0c1.1,0,1.2-0.4,1.2-1.3c0-1.3-0.3-2.6,0.4-3.9 C26.7,17.7,25.3,14,21.9,13.2z M8.1,11c0-0.5,0.5-0.5,0.9-0.5c1.4,0,2.7,0,4.1,0c1.4,0,2.7,0,4.1,0c0.4,0,0.9,0,0.9,0.5 c0,0.6-0.4,0.7-0.9,0.7c-2.7,0-5.4,0-8.2,0C8.5,11.7,8,11.6,8.1,11z M23.1,18.6c-0.2,1.2-1.3,2.2-2.6,2.2c-1.7-0.1-2.8-1-3.7-2.4 c-2.9,3.2-5,3.2-7.7-0.1c-0.8,1.3-1.8,2.2-3.2,2.4c-1.5,0.2-2.8-0.7-3-2.1c-0.2-1.5,0.6-2.7,2.2-3c0.3-0.1,0.6-0.1,0.9-0.1 c4.7,0,9.3,0,14,0c0.4,0,0.9,0.1,1.3,0.2C22.7,16.1,23.3,17.2,23.1,18.6z';
@@ -43,6 +43,9 @@ const Section_1_SVG_Paths = [{title:'新娘跟妆', id:'374',type:'tall', fillCo
                              {title:'婚纱礼服', id:'380',type:'tall', fillColor:'#fccb46', path:SVG_PATH_HSLF},
                              {title:'结婚图库', id:'0',type:'', fillColor:'#ae93e5', path:SVG_PATH_JHTK},];
 
+const homeTabTag = 'Home';
+const albumTabTag = 'Album';
+const merchantTabTag = 'Merchant';
 
 // 推荐页的8个选项
 export default class Channels extends Component {
@@ -81,7 +84,8 @@ export default class Channels extends Component {
                     colorOfCircle={item.fillColor}
                     onPress={()=>{
                       if (navigator) {
-                        this.props.navigator.push({
+                        navigator.push({
+                          id: albumTabTag,
                           message: '向右拖拽关闭页面',
                           sceneConfig: Navigator.SceneConfigs.FloatFromRight,
                         });
